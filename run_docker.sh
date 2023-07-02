7c0d0ffe93528a631d6e0d29329d9de575a23089
Brazier85 <github@berger-em.de>
2023-07-02 23:33:53 +0200

    fix(): Typo

--- run_docker.sh
+++ run_docker.sh
@@ -10,7 +10,7 @@ docker build -t personal_zoo .
 docker stop personal_zoo
 
 # remove old container
-docker rm personal zoo
+docker rm personal_zoo
 
 # Run new container
 docker run -d -p 5000:5000 -v ./data:/app/data --name personal_zoo personal_zoo
