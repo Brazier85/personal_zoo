5735a47dc493de56384fec195486bf5655e87479
Brazier85 <github@berger-em.de>
2023-07-02 23:32:42 +0200

    feat(): Startup script

---
+++ run_docker.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+# Get new data
+git pull
+
+# Built new container
+docker build -t personal_zoo .
+
+# Stop old container
+docker stop personal_zoo
+
+# remove old container
+docker rm personal zoo
+
+# Run new container
+docker run -d -p 5000:5000 -v ./data:/app/data --name personal_zoo personal_zoo
