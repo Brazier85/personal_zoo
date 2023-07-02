d3636daa88dc704f5ab5d6189de5436ce72b5ce0
Brazier85 <github@berger-em.de>
2023-07-02 23:35:03 +0200

    doc(): Added echos

--- run_docker.sh
+++ run_docker.sh
@@ -1,16 +1,23 @@
 #/bin/bash
 
 # Get new data
+echo "Pull new data"
 git pull
 
 # Built new container
+echo "built new container"
 docker build -t personal_zoo .
 
 # Stop old container
+echo "Stop old container"
 docker stop personal_zoo
 
 # remove old container
+echo "Remove old container"
 docker rm personal_zoo
 
 # Run new container
+echo "Start new container"
 docker run -d -p 5000:5000 -v ./data:/app/data --name personal_zoo personal_zoo
+
+echo "done"
